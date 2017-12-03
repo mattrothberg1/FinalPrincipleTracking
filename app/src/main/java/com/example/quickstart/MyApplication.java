10b22706ff748c7f593bdc6499c2b2736dfb75fd
mattrothberg1 <mdr218@lehigh.edu>
2017-12-03 17:27:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/quickstart/MyApplication.java
@@ -0,0 +1,25 @@
+package com.example.quickstart;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.estimote.coresdk.common.config.EstimoteSDK;
+
+//
+// Running into any issues? Drop us an email to: contact@estimote.com
+//
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        EstimoteSDK.initialize(getApplicationContext(), "quickstart-axi", "d04b82ac7e9d087c8d920478f8a13f7a");
+
+        // uncomment to enable debug-level logging
+        // it's usually only a good idea when troubleshooting issues with the Estimote SDK
+//        EstimoteSDK.enableDebugLogging(true);
+    }
+}
